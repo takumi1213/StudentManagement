39dbcded23ef21016cb40441dfbee470835b097b
1213takumi <shinnsekai5@gmail.com>
2024-10-21 21:50:52 +0900

    first commit

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,17 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Takumi Hamamoto" ;
+	private String age = "33" ;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello World!!!";
+	@GetMapping("/StudentInfo")
+	public String getStudentInfo(){
+		return name + " " + age + "歳";
 	}
+
+	@PostMapping("/StudentInfo")
+	public void setStudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/StudentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
+
+//	GET POST
+//	GET 取得する、リクエストの結果を受け取る
+//	POST 情報を与える
 }
