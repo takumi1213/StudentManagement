9a11cf003193a587c13faed13698035f079be3ae
1213takumi <shinnsekai5@gmail.com>
2024-10-31 15:25:36 +0900

    Question 23

---
+++ src/main/java/raisetech/StudentManagement/StudentCourse.java
@@ -0,0 +1,17 @@
+// 23回　課題
+package raisetech.StudentManagement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentCourse {
+
+  private String id;
+  private String studentId;
+  private String CourseName;
+  private String CourseStartAt;
+  private String CourseEndAt;
+
+}
